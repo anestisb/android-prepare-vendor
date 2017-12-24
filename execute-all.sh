7614906167cfca83eff13f9b8e17667b667dd961
Anestis Bechtsoudis <anestis@census-labs.com>
2017-12-24 10:08:37 +0200

    Revert fuse-ext2 for Linux until debugfs issue is resolved
    More info in #114
    
    Signed-off-by: Anestis Bechtsoudis <anestis@census-labs.com>

--- execute-all.sh
+++ execute-all.sh
@@ -320,8 +320,10 @@ if isDarwin; then
   SYS_TOOLS+=("umount")
   _UMOUNT=umount
 else
-  # For Linux use debugfs
-  USE_DEBUGFS=true
+  # For Linux use debugfs (TODO: Under investigation)
+  # USE_DEBUGFS=true
+  SYS_TOOLS+=("fusermount")
+  _UMOUNT="fusermount -u"
 fi
 
 # Check that system tools exist
